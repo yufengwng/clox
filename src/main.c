017ed439269c412468669c91ab78142712447721
Yufeng Wang <yufengwang05@gmail.com>
2019-06-08 00:07:16 -0400

    disassembler for debugging

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     chunk_init(&chunk);
     chunk_write(&chunk, OP_RETURN);
+    disassemble_chunk(&chunk, "test chunk");
     chunk_free(&chunk);
     return 0;
 }
